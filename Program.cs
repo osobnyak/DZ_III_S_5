2a67a74dcee24d176de4a59dbff1ae033c1864f2
Vladimir.S <vibiray2@mail.ru>
2023-01-15 23:22:28 +0400

    2 done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,42 @@
-﻿//
+﻿// Задайте массив вещественных чисел. Найдите разницу между max и min элементами массива.
+//v.0.1.my
+
+double[] num = new double[10];
+double max = 0;
+double min = 0;
+
+for (int i = 0; i < num.Length; i++)
+{
+    num[i] = (new Random().NextDouble() + new Random().Next(-10, 11));
+    Console.Write($"{Math.Round(num[i], 1)} ");
+    if (num[i] > max)
+    {
+        max = Math.Round(num[i], 1);
+    }
+    if (num[i] < min)
+    {
+        min = Math.Round(num[i], 1);
+    }
+}
+Console.WriteLine();
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
+
+
+
+
+
+
+
+
+
+
+
+//v.0.2.group 
+
+
+//*********************************************************************
+
+
